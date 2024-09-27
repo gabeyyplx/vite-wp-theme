5d3e41d5e5aee990d88888e2b1c73411faf0e53a
gabeyyclear <gabriel.gruenberg@icloud.com>
2024-09-27 16:43:32 +0200

    Add footer with widget areas and copyright menu

--- footer.php
+++ footer.php
@@ -1,5 +1,27 @@
-</div><!-- #page -->
-</div><!-- .wp-content -->
+</div> <!-- .content -->
+
+<footer class="footer">
+    <div class="footer-content">
+        <div class="footer-container">
+            <?php $theme = ViteWpTheme::get_instance();
+            $theme->display_footer_widgets(); ?>
+        </div>
+    </div>
+    <div class="copyright-area">
+        <div class="copyright-container">
+            <div class="copyright">
+                Copyright <?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?>
+            </div>
+            <div class="copyright-menu">
+                <?php wp_nav_menu(array(
+                    'theme_location' => 'copyright_menu',
+                    'fallback_cb' => false,
+                    'depth' => 1
+                )); ?>
+            </div>
+        </div>
+    </div>
+</footer>
 
 <?php wp_footer(); ?>
 
--- functions.php
+++ functions.php
@@ -1,22 +1,34 @@
 <?php 
 class ViteWpTheme {
+    private static $instance = null;
     private $theme_name = 'theme';
     private $manifest_path;
+    private $footer_columns = 4;
+
     private $theme_features = array(
         'title-tag',
         'custom-logo',
         'menus',
         'post-thumbnails'
     );
+
     private $menu_locations = array(
         'main-menu' => 'Main menu',
-        'footer-menu' => 'Footer menu'
+        'copyright-menu' => 'Copyright menu'
     );
 
-    public function __construct() {
+    public static function get_instance() {
+        if(self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {
         $this->manifest_path = get_template_directory() . "/dist/.vite/manifest.json";
         add_action('wp_enqueue_scripts', [$this, 'enqueue_assets']);
         add_action('after_setup_theme', [$this, 'setup_theme_features']);
+        add_action('widgets_init', [$this, 'init_widget_areas']);
         add_action('wp_head', [$this, 'add_admin_bar_css']);
     }
 
@@ -35,19 +47,16 @@ class ViteWpTheme {
         register_nav_menus($this->menu_locations);
     }
 
-    private function enqueue_built_assets() {
-        $manifest_file = file_get_contents($this->manifest_path);
-        $manifest_content = json_decode($manifest_file);
+    public function init_widget_areas() {
+        $this->register_footer_widgets();
+    }
 
-        $main_bundle = $manifest_content->{"src/main.js"}->file ?? null;
-        if($main_bundle) {
-            wp_enqueue_script("$this->theme_name-scripts", get_template_directory_uri() . '/dist/' . $main_bundle, [], false, true);
-        }
-
-        $main_style = $manifest_content->{"style.css"}->file ?? null;
-        if($main_style) {
-            wp_enqueue_style("$this->theme_name-style", get_template_directory_uri() . '/dist/' . $main_style);
-        }
+    public function display_footer_widgets() {
+        for($i = 1; $i <= $this->footer_columns; $i++): ?>
+            <ul class="footer-widget-column">
+                <?php dynamic_sidebar("footer-widget-$i"); ?>
+            </ul>
+        <?php endfor;
     }
 
     public function add_admin_bar_css() {
@@ -70,10 +79,35 @@ class ViteWpTheme {
         }
     }
 
+    private function enqueue_built_assets() {
+        $manifest_file = file_get_contents($this->manifest_path);
+        $manifest_content = json_decode($manifest_file);
+
+        $main_bundle = $manifest_content->{"src/main.js"}->file ?? null;
+        if($main_bundle) {
+            wp_enqueue_script("$this->theme_name-scripts", get_template_directory_uri() . '/dist/' . $main_bundle, [], false, true);
+        }
+
+        $main_style = $manifest_content->{"style.css"}->file ?? null;
+        if($main_style) {
+            wp_enqueue_style("$this->theme_name-style", get_template_directory_uri() . '/dist/' . $main_style);
+        }
+    }
+
     private function enqueue_dev_assets() {
         wp_enqueue_script_module('vite', 'http://localhost:5173/@vite/client');
         wp_enqueue_script_module("$this->theme_name-dev", 'http://localhost:5173/src/main.js');
     }
+
+    private function register_footer_widgets() {
+        for($i = 1; $i <= $this->footer_columns; $i++) {
+            register_sidebar(array(
+                    'name' => "Footer Widget $i",
+                    'id' => "footer-widget-$i"
+                )
+            );
+        }
+    }
 }
 
-new ViteWpTheme();
+ViteWpTheme::get_instance();
