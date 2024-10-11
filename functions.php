9cd445cb1b7f04727d13c9e330b1a02b3f3a588d
gabeyyclear <gabriel.gruenberg@icloud.com>
2024-10-11 16:42:56 +0200

    Migrate to FSE block theme

--- functions.php
+++ functions.php
@@ -1,21 +1,7 @@
-<?php 
-class ViteWpTheme {
+<?php class ViteWpTheme {
     private static $instance = null;
     private $theme_name = 'theme';
     private $manifest_path;
-    private $footer_columns = 4;
-
-    private $theme_features = array(
-        'title-tag',
-        'custom-logo',
-        'menus',
-        'post-thumbnails'
-    );
-
-    private $menu_locations = array(
-        'main-menu' => 'Main menu',
-        'copyright-menu' => 'Copyright menu'
-    );
 
     public static function get_instance() {
         if(self::$instance === null) {
@@ -27,9 +13,6 @@ class ViteWpTheme {
     private function __construct() {
         $this->manifest_path = get_template_directory() . "/dist/.vite/manifest.json";
         add_action('wp_enqueue_scripts', [$this, 'enqueue_assets']);
-        add_action('after_setup_theme', [$this, 'setup_theme_features']);
-        add_action('widgets_init', [$this, 'init_widget_areas']);
-        add_action('wp_head', [$this, 'add_admin_bar_css']);
     }
 
     public function enqueue_assets() {
@@ -37,45 +20,6 @@ class ViteWpTheme {
             $this->enqueue_built_assets();
         } else {
             $this->enqueue_dev_assets();
-        }
-    }
-
-    public function setup_theme_features() {
-        foreach($this->theme_features as $feature) {
-            add_theme_support($feature);
-        }
-        register_nav_menus($this->menu_locations);
-    }
-
-    public function init_widget_areas() {
-        $this->register_footer_widgets();
-    }
-
-    public function display_footer_widgets() {
-        for($i = 1; $i <= $this->footer_columns; $i++): ?>
-            <ul class="footer-widget-column">
-                <?php dynamic_sidebar("footer-widget-$i"); ?>
-            </ul>
-        <?php endfor;
-    }
-
-    public function add_admin_bar_css() {
-        if (is_admin_bar_showing()) {
-            echo '
-                <style>
-                    body nav.main-nav {
-                        top: 32px;
-                    }
-                    @media screen and (max-width: 782px) {
-                        body nav.main-nav {
-                            top: 46px;
-                        }
-                        #wpadminbar {
-                            position: fixed;
-                        }
-                    }
-                </style>
-            ';
         }
     }
 
@@ -98,16 +42,5 @@ class ViteWpTheme {
         wp_enqueue_script_module('vite', 'http://localhost:5173/@vite/client');
         wp_enqueue_script_module("$this->theme_name-dev", 'http://localhost:5173/src/main.js');
     }
-
-    private function register_footer_widgets() {
-        for($i = 1; $i <= $this->footer_columns; $i++) {
-            register_sidebar(array(
-                    'name' => "Footer Widget $i",
-                    'id' => "footer-widget-$i"
-                )
-            );
-        }
-    }
 }
-
 ViteWpTheme::get_instance();
--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-import './style.scss'
-
 document.addEventListener('DOMContentLoaded', () => {
-  initMobileNav()
+  console.log('Hello world')
 })
-
-const initMobileNav = () => {
-  const mobileNavToggle = document.querySelector('button.toggle-mobile-nav')
-  if (!mobileNavToggle) return
-
-  mobileNavToggle.addEventListener('click', () => {
-    const mainMenu = document.querySelector('.main-menu')
-    if (!mainMenu) return
-    mainMenu.classList.toggle('open')
-  })
-}
